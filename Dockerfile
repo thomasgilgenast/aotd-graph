thomasgilgenast/aotd-graph

commit 47dbc6b8fc842f71a0044c70363499c6919cef01

multiple changes
reorganized and extended README

matplotlibrc now gets moved to the Docker image in the same way that
requirements.txt is moved

command line output from build_graph.py is encoded before printing as a
quick fix to a possible bug related to unicode video titles

Dockerfile
@@ -15,10 +15,7 @@ RUN apt-get update && apt-get install --yes --no-install-recommends \
 
 # set working directory and copy files
 WORKDIR /aotd-graph
-ADD requirements.txt /aotd-graph
+ADD requirements.txt matplotlibrc /aotd-graph
 
 # install any needed packages specified in requirements.txt
 RUN pip3 install -r requirements.txt
-
-# set default backend in matplotlibrc
-RUN echo "backend: Cairo" >> matplotlibrc

build_graph.py
@@ -31,7 +31,7 @@ def explore(g, queue, visited):
             print('adding edge')
             g.add_edge(v, visited[el['href']])
         elif 'album' in el['title'].lower():
-            print('adding node to %s' % el['title'])
+            print('adding node to %s' % el['title'].encode('utf-8'))
             child = g.add_vertex()
             g.vp['url'][child] = el['href']
             g.vp['title'][child] = el['title']
@@ -75,7 +75,7 @@ def main():
         explore(g, queue, visited)
 
     # save to disk
-    g.save("aotd.gt")
+    g.save('aotd.gt')
 
 
 if __name__ == '__main__':

commit 14b4127091e412c94281724c1df0262651cd85c4

fix for Dockerfile
commit 47dbc6b broke it by incorrectly adding two files to the
/aotd-graph directory

Dockerfile
@@ -15,7 +15,7 @@ RUN apt-get update && apt-get install --yes --no-install-recommends \
 
 # set working directory and copy files
 WORKDIR /aotd-graph
-ADD requirements.txt matplotlibrc /aotd-graph
+ADD requirements.txt matplotlibrc /aotd-graph/
 
 # install any needed packages specified in requirements.txt
 RUN pip3 install -r requirements.txt